c0ae95c05b662afc317a7a9bb4f1ae6cd59a2240
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-21 13:54:06 +0700

    Basic Setup

---
+++ src/components/AppBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full h-[105px] bg-primary flex items-center justify-center">
+    <div class="container">
+      <h1 class="text-white text-2xl font-bold">TO DO LIST APP</h1>
+    </div>
+  </div>
+</template>
