2c2088abc87eaa20d68debea7346f549330c73cf
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-24 22:38:50 +0700

    cy lagi

--- src/components/modals/DeleteModal.vue
+++ src/components/modals/DeleteModal.vue
@@ -50,7 +50,7 @@ defineExpose({
 
 <template>
   <TransitionRoot appear :show="isOpen" as="template">
-    <Dialog as="div" @close="closeModal" class="relative z-10">
+    <Dialog as="div" @close="closeModal" class="relative z-10" data-cy="modal-delete">
       <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
         leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 bg-black bg-opacity-25" />
